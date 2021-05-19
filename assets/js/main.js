57e2a213d52cad4664456d3cbed865cca935ac08
stetaf <s.tafuri95@gmail.com>
2021-05-19 13:00:45 +0200

    Add function to remove task from the list

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,9 @@ const app = new Vue ({
         addTask() {
             this.tasks.push(this.inputTask);
             this.inputTask = '';
+        },
+        removeTask(index) {
+            this.tasks.splice(index, 1);
         }
     } 
 });
